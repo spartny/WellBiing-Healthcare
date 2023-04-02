fd21c804db934845d23618db3b7159f42ea4fe02
Arnav Ignatius <105193713+spartny@users.noreply.github.com>
2023-04-02 12:24:57 +0530

    Login Controller

--- src/main/java/com/wellbiing/wellbiinghealthcare/LoginController.java
+++ src/main/java/com/wellbiing/wellbiinghealthcare/LoginController.java
@@ -1,10 +1,31 @@
 package com.wellbiing.wellbiinghealthcare;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class LoginController {
-    
+    @FXML
+    private Button loginButton;
+    private TextField textfieldUsername;
+    private PasswordField passwordfield;
 
+    @FXML
+    void ButtonHoverEnd(MouseEvent event) {
+        Button b = (Button)event.getSource();
+        b.setStyle("-fx-background-color: #ffffff");
+        b.setStyle(("-fx-background-radius: 50px"));
+    }
+
+    @FXML
+    void ButtonHoverStart(MouseEvent event) {
+        Button b = (Button)event.getSource();
+        b.setStyle("-fx-background-color: #a4adb7");
+        b.setStyle(("-fx-background-radius: 50px"));
+    }
+    
 
 }
